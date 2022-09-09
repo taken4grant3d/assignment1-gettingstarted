17a6cb4be656fa04cd0be0bb8d1a1f379fbb70f8
imac <jv2378@nyu.edu>
2022-09-09 15:36:22 -0400

    test

--- solution.py
+++ solution.py
@@ -5,10 +5,25 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "mtls"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
+        answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 7
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
